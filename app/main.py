d19a88429bf90f0358733bb0f7b46dd4ed784f56
Amin-BSH <m.aminbesharatian@gmail.com>
2026-02-04 15:55:33 +0330

    changed the host and port in main.py
    Changed the host from 127.0.0.1 to 0.0.0.0 and port from 8000 to 8080.

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 if __name__ == "__main__":
     # Alert!!! take care about the reload
-    uvicorn.run(app="config:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app="config:app", host="0.0.0.0", port=8080, reload=True)
